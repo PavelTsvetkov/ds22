330583d9d3daa80f398c41a22a2d3a6413659066
Pavel Tsvetkov <tsvetkovpa@gmail.com>
2018-09-16 23:12:08 +0300

    RNN Keras Model initial commit

--- pavel/rnn_pipeline.py
+++ pavel/rnn_pipeline.py
@@ -6,11 +6,17 @@ from sklearn.metrics import f1_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import ExtraTreeClassifier
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.layers import Dense, Activation, Dropout, Embedding, LSTM
+from keras.models import Sequential
 
 import pavel.utils as u
-from pavel.utils import TFIDFVectorizer, BagOfWordsVectorizer
+from pavel.utils import TFIDFVectorizer, BagOfWordsVectorizer, SequenceVectorizer
 
 # ------------------- Configuration Section ---------------
+DABBLE_VOCAB = "C:\\tmp\\dabble\\vocab.dic"
+TRAINSET_FILE = "C:\\tmp\\dabble\\trainset.bin"
+MOVIES_METADATA_CSV = "C:\\tmp\\dabble\\movies_metadata.csv"
 show_confusion_matr = True
 
 FEATURE_COLUMN = "overview"
@@ -20,21 +26,15 @@ pre_process = u.pre_process
 detectClasses = u.detectClasses
 extract_classes = u.extract_classes
 
-classifier = MLPClassifier(verbose=True, early_stopping=True, max_iter=10, hidden_layer_sizes=(300, 300),
-                           tol=0.000001)  # F1=0.50
-# classifier = RandomForestClassifier(max_depth=1000,n_jobs=4,n_estimators=20)  # F1=0.30
-# classifier = ExtraTreeClassifier(max_depth=1000)  # F1=0.32
-# classifier = GaussianNB() #not working with simultaneous multiclass
-# vectorizer = TFIDFVectorizer(mx_features=10000)
-vectorizer = BagOfWordsVectorizer(mx_features=None, n_gram_range=(1, 1), minDf=10, maxDF=0.98)
+vectorizer = SequenceVectorizer(minDf=0.02, maxDf=0.98, file=DABBLE_VOCAB)
 
 # ------------------- Configuration Section ---------------
 
 print("Loading dataset")
-dataset = pd.read_csv("C:\\tmp\\dabble\\movies_metadata.csv")
+dataset = pd.read_csv(MOVIES_METADATA_CSV)
 
 print("Preprocessing")
-dataset = pre_process(dataset)  # lower case, cleanse, etc.
+dataset = pre_process(dataset[:100])  # lower case, cleanse, etc.
 
 print("Detecting classes")
 dataset = detectClasses(dataset, column=CLASS_COLUMN, prefix=CLASS_PREFIX)  # generates new columns, one per class
@@ -50,14 +50,12 @@ vectorizer.train(train[FEATURE_COLUMN])
 
 print("Vectorizing train input")
 train_x = vectorizer.vectorize(train[FEATURE_COLUMN])
-print("Train shape:", train_x.shape)
 
 print("Extracting train classes")
 train_classes, train_y = extract_classes(train, prefx=CLASS_PREFIX, classes=None)
 
 print("Vectorizing test input")
 test_x = vectorizer.vectorize(test[FEATURE_COLUMN])
-print("Test shape:", test_x.shape)
 
 print("Extracting test classes")
 tmp_classes, test_y = extract_classes(test, prefx=CLASS_PREFIX, classes=None)
@@ -66,23 +64,16 @@ dataset = None
 test = None
 train = None
 
-print("Training classifier")
-classifier.fit(train_x.toarray(), train_y)
+mdl = Sequential()
+mdl.add(Embedding((len(vectorizer.vocab)+1), 300, input_length=vectorizer.maxLen))
+mdl.add(LSTM(100))
 
-print("Testing classifier")
-pred_y = classifier.predict(test_x.toarray())
+mdl.add(Dense(train_y.shape[1], activation="sigmoid"))
 
-print("Confusion matrices")
-for i, c in enumerate(train_classes):
-    print("Class:", c, "F1 score:", f1_score(test_y[:, i], pred_y[:, i]))
-    if (show_confusion_matr):
-        conf_matr = confusion_matrix(test_y[:, i], pred_y[:, i])
-        print(conf_matr)
+mdl.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
 
-print("Total F1 score:", f1_score(test_y, pred_y, average='micro'))
+print(mdl.input_shape)
+mdl.summary()
 
-features = classifier.feature_importances_
-words = vectorizer.vec.vocabulary_
-
-sorted_w = sorted(zip(features, words), reverse=True, key=lambda item: item[0])
-print(sorted_w[:10])
+history = mdl.fit(train_x, train_y, epochs=1000, verbose=1, validation_split=0.2,
+                  batch_size=7)
--- pavel/utils.py
+++ pavel/utils.py
@@ -1,10 +1,15 @@
 import collections
 import json
 from collections import __init__
-
+import numpy as np
 import pandas as pd
 from nltk.corpus import stopwords
+from scipy.sparse import csc_matrix, lil_matrix
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+import pickle as pic
+import os
 
 MIN_WORD_LEN = 3
 
@@ -45,6 +50,15 @@ def count_words(words, word_counts):
             word_counts[w] = word_counts[w] + 1
 
 
+def token_gen(strng):
+    sentences = sent_tokenize(strng)
+    for sentence in sentences:
+        for word in word_tokenize(sentence):
+            if not (word in stopwords or len(word) < MIN_WORD_LEN):
+                yield word
+        yield "EOS"
+
+
 class FeatureVectorizer(object):
     def __init__(self) -> None:
         super().__init__()
@@ -54,6 +68,92 @@ class FeatureVectorizer(object):
 
     def train(self, column):
         pass
+
+
+def count_occurences(word, list):
+    i = 0
+    for item in list:
+        if word in item:
+            i = i + 1
+    return i
+
+
+keywords = set(["EOS", "UNK"])
+
+
+class SequenceVectorizer(FeatureVectorizer):
+
+    def __init__(self, file=None, minDf=0.0, maxDf=1.0, maxLen=300) -> None:
+        super().__init__()
+        self.maxLen = maxLen
+        self.maxDf = maxDf
+        self.minDf = minDf
+        self.file = file
+
+    def vectorize(self, column):
+        result = np.full((len(column), self.maxLen), len(self.vocab))
+
+        for i, item in enumerate(column):
+            for k, token in enumerate(token_gen(item)):
+                if token in self.vocab:
+                    idx = self.vocab[token]
+                else:
+                    idx = self.vocab["UNK"]
+                result[i, k] = idx
+        return result
+
+    def train(self, column):
+        self.vocab = None
+
+        if self.file is not None:
+            self.vocab = self.load_from_file()
+
+        if self.vocab is None:
+            allwords = self.register_words(column)
+            matr = self.register_word_distribution(allwords, column)
+            bc = matr.sum(axis=0)
+            minCount = self.minDf * len(column)
+            maxCount = self.maxDf * len(column)
+            indices_to_keep = self.select_indices(bc, maxCount, minCount)
+
+            words_filtered = set(
+                [word for word, idx in allwords.items() if (idx in indices_to_keep or word in keywords)])
+            words_filtered.add("UNK")
+            self.vocab = {word: idx for idx, word in enumerate(words_filtered)}
+            self.save_to_file()
+
+    def select_indices(self, bc, maxCount, minCount):
+        indices_to_keep = set()
+        it = np.nditer(bc[0], flags=['f_index'])
+        while not it.finished:
+            if (it[0] >= minCount and it[0] <= maxCount):
+                indices_to_keep.add(it.index)
+            it.iternext()
+        return indices_to_keep
+
+    def register_word_distribution(self, allwords, column):
+        matr = lil_matrix((len(column), len(allwords)))
+        for idx, item in enumerate(column):
+            for token in token_gen(item):
+                matr[idx, allwords[token]] = 1
+        return matr
+
+    def register_words(self, column):
+        allwords = {}
+        for item in column:
+            for token in token_gen(item):
+                if not (token in allwords):
+                    allwords[token] = len(allwords)
+        return allwords
+
+    def load_from_file(self):
+        if os.path.isfile(self.file):
+            with open(self.file, mode="rb") as f:
+                return pic.load(f)
+
+    def save_to_file(self):
+        with open(self.file, mode="wb") as f:
+            pic.dump(self.vocab, f, pic.HIGHEST_PROTOCOL)
 
 
 class TFIDFVectorizer(FeatureVectorizer):
@@ -70,9 +170,9 @@ class TFIDFVectorizer(FeatureVectorizer):
 
 class BagOfWordsVectorizer(FeatureVectorizer):
 
-    def __init__(self, mx_features=5000) -> None:
+    def __init__(self, mx_features=5000, minDf=40, maxDf=0.01) -> None:
         super().__init__()
-        self.vec = CountVectorizer(stop_words="english", max_features=mx_features)
+        self.vec = CountVectorizer(stop_words="english", max_features=mx_features, min_df=minDf, max_df=maxDf)
 
     def vectorize(self, column):
         return self.vec.transform(column)
@@ -89,9 +189,11 @@ def detectClasses(df, column=None, prefix=None):
 
 
 def pre_process(df):
+    df = df.dropna(subset=["overview"])
+    df = df.reset_index(drop=True)
     df['genres'] = df['genres'].apply(rectify_json)
-    df["overview"] = df["overview"].fillna(value="")
     df['overview'] = df['overview'].apply(lowercase)
+    return df
 
 
 def extract_classes(data, prefx, classes):
